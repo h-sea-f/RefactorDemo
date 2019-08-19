fec5477b1719abd030ae434d7c799affe90e547c
h-sea-f <Hehaifeng2019>
2019-08-19 18:29:23 +0800

    add should_return_original_items_when_invoke_updateQuality_and_item_name_a_quality_0 test

--- src/test/java/GildedRose/GildedRoseTest.java
+++ src/test/java/GildedRose/GildedRoseTest.java
@@ -15,5 +15,11 @@ public class GildedRoseTest {
         assertEquals(1, gildedRose.getItems()[0].getQuality());
     }
 
-
+    @Test
+    public void should_return_original_items_when_invoke_updateQuality_and_item_name_a_quality_0(){
+        Item[] items = {new Item("a", 2, 0)};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertEquals(items[0].toString(), gildedRose.getItems()[0].toString());
+    }
 }
